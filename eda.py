eff92ee9c652766c4bee2f3afad86aeda8e90abd
Amanda Coto <amiscj25@gmail.com>
2018-10-03 18:45:02 -0600

    Contextualizacion de la base

---
+++ eda.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 30 23:00:02 2018
+
+@author: Amanda
+"""
+
+import pandas as pd
+datos201807=pd.read_csv("C:/Users/Eric/Documents/GitHub/data/201807-fordgobike-tripdata.csv",sep=',')
+datos201806=pd.read_csv("201806-fordgobike-tripdata.csv",sep=',')
+datos201805=pd.read_csv("201805-fordgobike-tripdata.csv",sep=',')
+datos201804=pd.read_csv("201804-fordgobike-tripdata.csv",sep=',')
+datos201803=pd.read_csv("201803-fordgobike-tripdata.csv",sep=',')
+datos201802=pd.read_csv("201802-fordgobike-tripdata.csv",sep=',')
+datos201801=pd.read_csv("201801-fordgobike-tripdata.csv",sep=',')
+datos2017=pd.read_csv("2017-fordgobike-tripdata.csv",sep=',')
+
+
+
